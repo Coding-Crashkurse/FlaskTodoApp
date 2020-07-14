455b4f361e9e4ecb2be42f0e0816b03b8323e4bc
markus <datamastery87@gmail.com>
2020-07-14 18:45:04 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,169 @@
+from flask import Flask, request
+from flask_restx import Resource, Api, fields
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, User
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (
+    JWTManager,
+    jwt_required,
+    create_access_token,
+    get_jwt_identity,
+)
+from flask_cors import CORS, cross_origin
+import re
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+from itsdangerous import URLSafeTimedSerializer, BadTimeSignature, SignatureExpired
+
+app = Flask(__name__)
+app.secret_key = "markus"
+setup_db(app, database_path="postgresql://markus:test@134.122.78.140:5432/register")
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+api = Api(app, version="1.0", title="TodoMVC API", description="A simple TodoMVC API",)
+bcrypt = Bcrypt()
+
+s = URLSafeTimedSerializer(app.secret_key)
+
+ns = api.namespace("todos", description="TODO operations")
+
+# https://stackoverflow.com/questions/49226806/python-check-for-a-valid-email
+def is_valid_email(email):
+    if len(email) > 7:
+        return bool(
+            re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email)
+        )
+
+
+@api.route("/register")
+class UserRegister(Resource):
+    def post(self):
+        """Allows a new user to register with his email and password"""
+
+        error = False
+        email = request.get_json()["email"]
+
+        if is_valid_email(email) is False:
+            return {"message": "Invalid Email"}, 409
+
+        password = request.get_json()["password"]
+
+        filtereduser = User.query.filter_by(email=email).one_or_none()
+        if filtereduser is not None:
+            return {"message": "User already exists"}, 401
+
+        try:
+            password = bcrypt.generate_password_hash(password).decode("utf8")
+            newuser = User(email=email, password=password)
+            newuser.insert()
+        except:
+            error = True
+            db.session.rollback()
+            return {"error": 404}, 404
+        finally:
+            db.session.close()
+        if error is False:
+            return {"success": 200}, 201
+
+
+@api.route("/login")
+class UserLogin(Resource):
+    def post(self):
+        """Allows a new user to login with his email and password"""
+
+        email = request.get_json()["email"]
+        password = request.get_json()["password"]
+
+        user = User.query.filter_by(email=email).one_or_none()
+        user = user.format()
+
+        if user is None:
+            return {"message": "user does not exist"}, 404
+
+        if bcrypt.check_password_hash(pw_hash=user["password"], password=password):
+            if user["active"]:
+                access_token = create_access_token(identity=user)
+                return (
+                    {
+                        "user": user,
+                        "access_token": access_token,
+                        "message": "Login successful",
+                    },
+                    200,
+                )
+
+            else:
+                return {"message": "User not activated"}, 400
+
+        else:
+            return {"message": "Wrong credentials"}, 401
+
+
+@api.route("/confirm")
+class Confirm(Resource):
+    def post(self):
+        email = request.get_json()["email"]
+
+        user = User.query.filter_by(email=email).one_or_none()
+        if user is None:
+            return {"message": "User not found"}
+
+        token = s.dumps(email, salt="email-confirmation")
+
+        EMAIL_ADDRESS = os.environ.get("MAIL_USERNAME")
+        EMAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+        msg = EmailMessage()
+        msg["Subject"] = "Confirmation email"
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = email
+
+        msg.set_content(
+            "Please copy this confirmation link into your browser: localhost:5000/confirm/"
+            + token
+        )
+
+        with smtplib.SMTP_SSL(
+            os.environ.get("MAIL_SERVER"), os.environ.get("MAIL_PORT")
+        ) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+        return {"message": "mail sent successful"}
+
+
+@api.route("/confirm/<token>")
+class ConfirmToken(Resource):
+    def get(self, token):
+
+        try:
+            email = s.loads(token, salt="email-confirmation", max_age=200)
+            user = User.query.filter_by(email=email).one_or_none()
+            print(user.format())
+
+            if user is None:
+                return {"message": "user does not exist"}, 404
+            elif user.active:
+                return {"message": "user already activated"}, 403
+            else:
+                user.active = True
+                user.update()
+                return {"message": "Activation successful"}, 200
+
+        except BadTimeSignature:
+            return {"message": "Invalid signature"}, 403
+        except SignatureExpired:
+            return {"message": "Link expired"}, 404
+
+
+@api.route("/delete/<int:id>")
+class DeleteUser(Resource):
+    def get(self, id):
+        User.query.filter_by(id=id).one_or_none().delete()
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+
---
+++ models.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Boolean, Integer, String, DateTime, Column
+import datetime
+
+
+def setup_db(app, database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["TESTING"] = True
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = db.Column(String(256))
+    password = db.Column(String(256))
+    active = db.Column(Boolean, default=False)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password,
+            "active": self.active,
+            "created_date": self.created_date.strftime("%Y-%m-%d %H:%M:%S"),
+        }
